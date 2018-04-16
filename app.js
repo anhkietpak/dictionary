ab8d1a5c04969ac67d879457baf29a9d9cff3098
khalito <skscharaf@gmail.com>
2018-04-16 21:17:20 +0200

    getFruits as argument in response.send

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 var express = require('express');
 var nano = require('nano')('http://localhost:5984');
+var prom = require('nano-promises');
 var fruits = nano.db.use("fruits");
-
 var app = express();
 
-nano.db.list(function(err, body) {
-  	// body is an array
-//	console.log("datbases: " + body)
-});
-
+function getFruits(db) {
+	var listOfFruits = ["test"];
+	db.list(function(err, body) {
+		if (!err) {
+			body.rows.forEach(function(doc) {
+				db.get(doc.id, function(err, body) {
+					if (body.name) {
+						//console.log(body.name);
+						listOfFruits.push(body.name);
+						console.log(listOfFruits);
+					};
+				});
+			});
+		};
+	})
+	return listOfFruits;
+}
 
 app.get('/', function(request, response) {
-	var listOfFruits = ["test", "another"];
-	var returnFruits = function(data, cb) {
-		data.list(function(err, body) {
-			if (!err) {
-				body.rows.forEach(function(doc) {
-					data.get(doc.id, function(err, body) {
-						if (body.name) {
-							console.log(body.name);
-							listOfFruits.push(body.name);
-							console.log(listOfFruits);
-						};
-					});
-				});
-			};
-		});
-		cb(listOfFruits);
-	};
-	returnFruits(fruits, cb => response.send(cb));
+	response.send(getFruits(fruits));
 });
-
 
 var server = app.listen(3000, function() {
 	console.log("Running on port 3000")
